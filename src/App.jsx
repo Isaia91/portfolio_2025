8302e6634e63202db3acd0c606ab8044f9c55340
Isaïa Maperi <isaia.maperi@gmail.com>
2025-04-24 06:45:07 +0000

    swapping branch for ovh server

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+
 function App() {
     return (
-        <BrowserRouter>
-            <div className="flex flex-col min-h-screen">
-                <main className="flex-grow">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                </main>
-            </div>
-        </BrowserRouter>
+        <div className="flex flex-col min-h-screen">
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
